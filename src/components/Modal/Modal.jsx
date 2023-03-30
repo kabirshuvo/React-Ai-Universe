232e2af02858be2743ac32c0c2c46e951bb0c4a1
kabirshuvo <kabirshuvo19@gmail.com>
2023-03-30 07:01:01 +0600

    Intigrations are now displaying

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,28 +2,34 @@ import React from 'react';
 
 const Modal = (props) => {
    // console.log(props.ai);
-     const {accuracy, tool_name, image_link, website, description, integrations, featcher } = props.ai;
-     //console.log(image_link[0]);
+     const {accuracy, tool_name, image_link, website, description, integrations, features } = props.ai;
+     //console.log(integrations);
     return (
 <>
-            
                 <input type="checkbox" id="my-modal-5" className="modal-toggle" />
-
 
                 <div className="modal">
                     <div className="modal-box w-11/12 max-w-5xl">
                     <div className="card lg:card-side bg-base-100">
                     
-                    
                 <div className="card-body">
                     <h2 className="card-title w-96">{description? description : 'Not Found'}</h2>
 
-
-                    <div>
+                    <div className='flex justify-between'>
+                        <div>
                         <p className='text-2xl'>Features</p>
+                        {
+                            Object.values(features || {}).map(value => <p>{value.feature_name}</p>)
+                        }
+                        </div>
+                        <div>
                         <p className='text-2xl'>Intigrations</p>
+                        
+                        {integrations &&
+                        integrations.map(int => <p>{int? int : 'no int'}</p>)}
+                        
+                        </div>
                     </div>
-                    
                     
                 </div>
                     <figure>
